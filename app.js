44c3053c43485e47fc2a32e9b718a05bef9ebbfa
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-12-15 16:42:36 +0530

    Color flipper project completed

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+//Write JavaScript Code below this line
+
+//Variables
+const span = document.querySelector('span.color');
+const btn = document.querySelector('button#btn');
+let index = 0;
+
+//Event Listeners
+window.addEventListener('load', () => {
+  document.body.style.backgroundColor = colors[3];
+  span.textContent = colors[3];
+});
+
+//Event Listener Callback function
+const handleClick = () => {
+  document.body.style.backgroundColor = colors[index];
+  span.textContent = colors[index];
+  index++;
+  if (index > 3) {
+    index = 0;
+    return index;
+  }
+};
+
+btn.addEventListener('click', handleClick);
--- hex.js
+++ hex.js
@@ -1,1 +1,29 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+//Variables
+const span = document.querySelector('span.color');
+const btn = document.querySelector('button#btn');
+
+//turn repeating code into a function
+const hexCode = () => {
+  const r1 = Math.floor(Math.random() * 16);
+  const r2 = Math.floor(Math.random() * 16);
+  const r3 = Math.floor(Math.random() * 16);
+  const r4 = Math.floor(Math.random() * 16);
+  const r5 = Math.floor(Math.random() * 16);
+  const r6 = Math.floor(Math.random() * 16);
+  const hexCode = `#${hex[r1]}${hex[r2]}${hex[r3]}${hex[r4]}${hex[r5]}${hex[r6]}`;
+
+  return hexCode;
+};
+
+//EventListeners
+window.addEventListener('load', () => {
+  document.body.style.backgroundColor = hexCode();
+  span.textContent = hexCode();
+});
+
+btn.addEventListener('click', () => {
+  document.body.style.backgroundColor = hexCode();
+  span.textContent = hexCode();
+});
